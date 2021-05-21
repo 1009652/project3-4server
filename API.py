f53934006c6bffc7e24f4fda690fe3ddd720c303
Jurre van Wamelen <1009652@hr.nl>
2021-05-21 23:23:17 +0200

    added CheckIfRegisterd function

--- API.py
+++ API.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
 
 app = Flask(__name__)
 api = Api(app)
 
-class Withdraw(Resource): # GET
-    def get(self):
+db = MySQLdb.connect(host="remotemysql.com", user="5J9rC1RF8E", passwd="U8IIWXIJZT", db="5J9rC1RF8E")
+
+cursor = db.cursor()
+
+class CheckIfRegistered(Resource): # POST
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('IBAN', required=True)
+        args = parser.parse_args()
         try:
-            #do stuff here
-            newBalance = 300 - 70
-            return {'data': "Hello Matthijs"}, 200
+            data = str(args.get('IBAN'))
+            query = "SELECT iban FROM accounts WHERE iban = %s;"
+            cursor.execute(query, data)
+            if(data):
+                return {}, 208
+            else :
+                return {}, 433
         except:
             return {'error': 'no data available'}, 400
     pass
@@ -27,7 +41,7 @@ class Withdraw2(Resource): # POST
             return {'error': 'invalid input'}, 400 # Bad request
     pass
     
-api.add_resource(Withdraw, '/withdrawGet') 
+api.add_resource(CheckIfRegistered, '/checkIfRegistered') 
 api.add_resource(Withdraw2, '/withdrawPost') 
 
 if __name__ == '__main__':
