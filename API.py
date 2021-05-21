bde9aea1fe84a554508d93a3cff025fa966be0fa
1009652 <1009652@hr.nl>
2021-05-21 23:09:31 +0200

    modified API.py file

--- API.py
+++ API.py
@@ -9,7 +9,7 @@ class Withdraw(Resource): # GET
         try:
             #do stuff here
             newBalance = 300 - 70
-            return {'data': newBalance}, 200
+            return {'data': "Hello Matthijs"}, 200
         except:
             return {'error': 'no data available'}, 400
     pass
@@ -17,13 +17,12 @@ class Withdraw(Resource): # GET
 class Withdraw2(Resource): # POST
     def post(self):
         parser = reqparse.RequestParser() 
-        parser.add_argument('money', required=True)
+        parser.add_argument('name', required=True)
         args = parser.parse_args()
         try:
             # do stuff here
-            money = int(args.get('money'))
-            newBalance = 300 - money
-            return {'data' : newBalance}, 200 # OK
+            name = "Hello " + str(args.get('name'))
+            return {'data' : name}, 200 # OK
         except:
             return {'error': 'invalid input'}, 400 # Bad request
     pass
@@ -32,4 +31,4 @@ api.add_resource(Withdraw, '/withdrawGet')
 api.add_resource(Withdraw2, '/withdrawPost') 
 
 if __name__ == '__main__':
-    app.run() 
+    app.run(host='0.0.0.0', port=8050) 
